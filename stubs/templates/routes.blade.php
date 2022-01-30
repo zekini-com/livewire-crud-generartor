0c5f90f9ccc5830e62009fc7d414ac34dc48e714
llew <llewellyn@zekini.com>
2022-01-30 10:02:51 +0200

    29-invalid-route

--- stubs/templates/routes.blade.php
+++ stubs/templates/routes.blade.php
@@ -5,7 +5,7 @@
 Route::middleware(['auth:' . config('zekini-admin.defaults.guard')])->group(static function () {
     Route::prefix('admin')->name('admin/')->group(static function() {
         Route::prefix('{{ $resource }}')->name('{{ $resource }}/')->group(static function() {
-            Route::get('/', App\Http\Livewire\{{$resourcePlural}}\{{$resourcePlural}}::class)->name('index');
+            Route::get('/', App\Http\Livewire\{{$modelVariableName}}\{{$modelVariableName}}::class)->name('index');
         });
     });
 });
