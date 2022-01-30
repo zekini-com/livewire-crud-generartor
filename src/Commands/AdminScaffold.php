73d7f4de900340f02250067fd5b49a9975985920
llew <llewellyn@zekini.com>
2022-01-30 07:23:14 +0200

    28-spatie-permission

--- src/Commands/AdminScaffold.php
+++ src/Commands/AdminScaffold.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Zekini\CrudGenerator\Commands;
 
 use Illuminate\Console\Command;
@@ -8,20 +9,19 @@ use Zekini\CrudGenerator\Helpers\Utilities;
 
 class AdminScaffold extends Command
 {
-
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'admin:scafold';
+    protected $signature = 'admin:scaffold';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This command scafolds the admin with permissions for CRUD';
+    protected $description = 'This command scaffolds the admin with permissions for CRUD';
 
     /**
      * Create a new command instance.
@@ -31,7 +31,6 @@ class AdminScaffold extends Command
     public function __construct()
     {
         parent::__construct();
-
     }
 
     /**
@@ -43,12 +42,12 @@ class AdminScaffold extends Command
     {
         // Setup the zekiniAdmin authentication guard
         $this->setupAdminAuthGuard();
-        
-        // // Create file migration for the default admin user
+
+        // Create file migration for the default admin user
         $this->publishVendors();
-      
+
         $this->info("Email : support@zekini.com");
-        $this->info('Password : '. config('zekini-admin.defaults.default-password'));
+        $this->info('Password : ' . config('zekini-admin.defaults.default-password'));
 
         // migrate generated tables
         $this->call('migrate');
@@ -56,103 +55,98 @@ class AdminScaffold extends Command
         $this->generateDefaultModelCruds();
 
         //call jetstream installation
-        $this->call('jetstream:install', ['stack'=> 'livewire']);
-        
+        $this->call('jetstream:install', ['stack' => 'livewire']);
+
         return Command::SUCCESS;
     }
 
     protected function generateDefaultModelCruds()
     {
-        $this->call('admin:crud:generate', ['table'=> 'zekini_admins', '--user'=>true]);
-        $this->call('admin:crud:generate', ['table'=> 'permissions']);
-        $this->call('admin:crud:generate', ['table'=> 'roles']);
-        $this->call('admin:crud:generate', ['table'=> config('activitylog.table_name'), '--readonly'=>true]);
-
+        $this->call('admin:crud:generate', ['table' => 'zekini_admins', '--user' => true]);
+        $this->call('admin:crud:generate', ['table' => 'permissions']);
+        $this->call('admin:crud:generate', ['table' => 'roles']);
+        $this->call('admin:crud:generate', ['table' => config('activitylog.table_name'), '--readonly' => true]);
     }
 
-    
     /**
      * Publish All vendors
      *
      * @return void
      */
-    protected function publishVendors()
+    protected function publishVendors(): void
     {
         $this->publishSpatiePermissionVendor();
 
         $this->publishSpatieLogVendor();
 
         $this->publishZekini();
-
     }
-    
+
     /**
      * Publishes Spatie Vendors
      *
      * @return void
      */
-    protected function publishSpatiePermissionVendor()
+    protected function publishSpatiePermissionVendor(): void
     {
-         //Spatie Permission
-         $this->call('vendor:publish', [
+        //Spatie Permission
+        $this->info('Publish Spatie Permission');
+        $this->call('vendor:publish', [
             '--provider' => 'Spatie\\Permission\\PermissionServiceProvider',
             '--tag' => 'migrations'
         ]);
-       
+
         $this->call('vendor:publish', [
             '--provider' => 'Spatie\\Permission\\PermissionServiceProvider',
             '--tag' => 'config'
         ]);
-
     }
 
-     /**
+    /**
      * Publishes Audit Vendor
      *
      * @return void
      */
-    protected function publishSpatieLogVendor()
+    protected function publishSpatieLogVendor(): void
     {
-         //Spatie Permission
-         $this->call('vendor:publish', [
+        //Spatie Activitylog
+        $this->info('Publish Spatie Activitylog');
+        $this->call('vendor:publish', [
             '--provider' => "Spatie\Activitylog\ActivitylogServiceProvider",
             '--tag' => 'activitylog-migrations'
         ]);
-       
+
         $this->call('vendor:publish', [
             '--provider' => "Spatie\Activitylog\ActivitylogServiceProvider",
             '--tag' => 'activitylog-config'
         ]);
-
     }
-    
+
     /**
      * Publish Zekini specific
      *
      * @return void
      */
-    protected function publishZekini()
+    protected function publishZekini(): void
     {
-
+        $this->info('Publish Zekini');
         $tags = ['config', 'migrations', 'resources', 'views', 'controllers'];
 
-        foreach($tags as $tag){
+        foreach ($tags as $tag) {
             $this->call('vendor:publish', [
-                '--provider'=> 'Zekini\\CrudGenerator\\LivewireCrudGeneratorServiceProvider',
-                '--tag'=> $tag
+                '--provider' => 'Zekini\\CrudGenerator\\LivewireCrudGeneratorServiceProvider',
+                '--tag' => $tag
             ]);
         }
-
-       
     }
 
-    
+
     /**
      * We try to setup the admin guards for authentication same thing we will do manually
      *
      * @return void
      */
-    protected function setupAdminAuthGuard()
+    protected function setupAdminAuthGuard(): void
     {
         $pathToFile = config_path('auth.php');
 
@@ -162,6 +156,7 @@ class AdminScaffold extends Command
             \'driver\' => \'session\',
             \'provider\' => \'zekini_admins\'
         ],';
+
         Utilities::strReplaceInFile($pathToFile, $find, $replaceWith);
 
         $find = '\'providers\' => [';
@@ -170,6 +165,7 @@ class AdminScaffold extends Command
             \'driver\' => \'eloquent\',
             \'model\' => Zekini\CrudGenerator\Models\ZekiniAdmin::class
         ],';
+
         Utilities::strReplaceInFile($pathToFile, $find, $replaceWith);
 
         $find = '\'passwords\' => [';
@@ -180,6 +176,7 @@ class AdminScaffold extends Command
             \'expire\' => 60,
             \'throttle\' => 60
         ],';
+
         Utilities::strReplaceInFile($pathToFile, $find, $replaceWith);
     }
 }
